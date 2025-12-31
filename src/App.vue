7ce2568ea02f8ddeac470d9b9a4497ebd7e987fc
rgirodon <remy.girodon@gmail.com>
2025-12-31 12:18:00 +0100

    Implement activity 1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Author from "./Author.js";
+
+export default {
+
+  data() {
+    let arthurRimbaud = new Author("Arthur", "Rimbaud");
+
+    return {
+      author: arthurRimbaud
+    }
+  }
+}
+</script>
+
+<template>
+  <h1> My Favourite Author </h1>
+  <div>
+    <label>First Name : </label>{{ author.firstName }}
+  </div>
+  <div>
+    <label>Last Name : </label>{{ author.lastName }}
+  </div>
+</template>
+
+<style scoped>
+</style>
